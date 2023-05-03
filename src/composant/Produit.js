0364eeef529732e35c5efce99b256766a0a914a6
philippedavyncho <nchodavyphilippe@gmail.com>
2023-05-03 00:08:17 +0000

    fil commit

--- src/composant/Produit.js
+++ src/composant/Produit.js
@@ -24,7 +24,7 @@ export default function Produit({name, price, image, item, id}){
                 <div className="card">
                   <Link to={`/${id}`}>
                   <div className="text-center">
-                    <img src={image} className="card-img-top d-none d-md-block img-fluid" alt={name} style={{width:"150px"}}/>
+                    <img src={image} className="card-img-top d-none d-md-inline img-fluid" alt={name} style={{width:"150px"}}/>
                   </div>
             
                   <img src={image} className="card-img-top d-md-none w-300" alt={name}/>
