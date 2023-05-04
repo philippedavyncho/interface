96386e25a40287c373bcad728a7a8cc38c4fb694
philippedavyncho <nchodavyphilippe@gmail.com>
2023-05-04 14:44:33 +0000

    slide3 commit

--- src/composant/Slider.js
+++ src/composant/Slider.js
@@ -1,9 +1,9 @@
-import ange1 from '../ange1.jpg'
-import ange2 from '../ange2.jpg'
-import ange3 from '../ange3.jpg'
-import ange4 from '../ange4.jpg'
+import banniere1 from '../banniere1.png'
+import banniere2 from '../banniere2.png'
+import banniere3 from '../banniere3.png'
+import banniere4 from '../banniere4.png'
 
-import Carousel from 'react-bootstrap/Carousel';
+/*import Carousel from 'react-bootstrap/Carousel';
 
 function CarouselFadeExample() {
   return (
@@ -44,5 +44,44 @@ function CarouselFadeExample() {
   );
 }
 
-export default CarouselFadeExample;
+export default CarouselFadeExample;*/
+
+import Carousel from 'react-bootstrap/Carousel';
+
+function UncontrolledExample() {
+  return (
+    <Carousel indicators={false} nextIcon={false} prevIcon={false} interval={1000}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="{banniere1}"
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="{banniere2}"
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="{banniere3}"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="{banniere4}"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default UncontrolledExample;
 
