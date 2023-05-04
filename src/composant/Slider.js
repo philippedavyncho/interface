0061fa15a9bb97423b3646236c695195f07063e2
philippedavyncho <nchodavyphilippe@gmail.com>
2023-05-04 14:49:41 +0000

    banniere commit

--- src/composant/Slider.js
+++ src/composant/Slider.js
@@ -3,7 +3,7 @@ import banniere2 from '../banniere2.png'
 import banniere3 from '../banniere3.png'
 import banniere4 from '../banniere4.png'
 
-/*import Carousel from 'react-bootstrap/Carousel';
+import Carousel from 'react-bootstrap/Carousel';
 
 function CarouselFadeExample() {
   return (
@@ -11,7 +11,7 @@ function CarouselFadeExample() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={ange1}
+          src={banniere1}
           alt="First slide"
           
         />
@@ -19,7 +19,7 @@ function CarouselFadeExample() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={ange2}
+          src={banniere2}
           alt="Second slide"
         />
 
@@ -27,14 +27,14 @@ function CarouselFadeExample() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={ange3}
+          src={banniere3}
           alt="Third slide"
         />
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={ange4}
+          src={banniere4}
           alt="Third slide"
         />
       </Carousel.Item>
@@ -44,44 +44,5 @@ function CarouselFadeExample() {
   );
 }
 
-export default CarouselFadeExample;*/
-
-import Carousel from 'react-bootstrap/Carousel';
-
-function UncontrolledExample() {
-  return (
-    <Carousel indicators={false} nextIcon={false} prevIcon={false} interval={1000}>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="{banniere1}"
-          alt="First slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="{banniere2}"
-          alt="Second slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="{banniere3}"
-          alt="Third slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="{banniere4}"
-          alt="Third slide"
-        />
-      </Carousel.Item>
-    </Carousel>
-  );
-}
-
-export default UncontrolledExample;
+export default CarouselFadeExample;
 
